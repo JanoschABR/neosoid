47676f1f6d85472d7c92840c4a65300bf992936d
JanoschABR <janoschabr@gmail.com>
2022-12-24 23:45:11 +0100

    Changed AssemblyInfo

--- Neosoid/Properties/AssemblyInfo.cs
+++ Neosoid/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Neosoid")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Pulsoid WebSocket provider for Neos")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("JanoschR")]
 [assembly: AssemblyProduct("Neosoid")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
